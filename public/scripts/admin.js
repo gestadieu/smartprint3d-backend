fd3fc08bc48bed2c655f35e65b4a736ce458a260
Gerald Estadieu <gestadieu@gmail.com>
2020-06-08 23:23:12 +0800

    fix dropdowns hide, show the deleted flag

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,7 +1,35 @@
-let dropdowns = document.querySelectorAll(".dropdown");
-dropdowns.forEach((dropdown) => {
-  dropdown.addEventListener("click", function (event) {
-    event.stopPropagation();
-    dropdown.classList.toggle("is-active");
+// let dropdowns = document.querySelectorAll(".dropdown");
+// dropdowns.forEach((dropdown) => {
+//   dropdown.addEventListener("click", function (event) {
+//     event.stopPropagation();
+//     dropdown.classList.toggle("is-active");
+//   });
+// });
+
+const dropdowns = document.querySelectorAll(".dropdown:not(.is-hoverable)");
+
+if (dropdowns.length > 0) {
+  dropdowns.forEach(function (el) {
+    el.addEventListener("click", function (e) {
+      e.stopPropagation();
+      el.classList.toggle("is-active");
+    });
   });
+
+  document.addEventListener("click", function (e) {
+    closeDropdowns();
+  });
+}
+
+function closeDropdowns() {
+  dropdowns.forEach(function (el) {
+    el.classList.remove("is-active");
+  });
+}
+
+document.addEventListener("keydown", function (event) {
+  let e = event || window.event;
+  if (e.key === "Esc" || e.key === "Escape") {
+    closeDropdowns();
+  }
 });
