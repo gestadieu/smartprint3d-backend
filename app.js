cb583664a40d980728b548b109f0ab1330c5bbb7
Gerald Estadieu <gestadieu@gmail.com>
2020-06-20 01:12:34 +0800

    start redis session storage again

--- app.js
+++ app.js
@@ -2,22 +2,23 @@ require("dotenv").config();
 var path = require("path");
 const express = require("express");
 const session = require("express-session");
-const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const flash = require("connect-flash");
+// const cookieParser = require("cookie-parser");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const User = require("./src/models/User");
-const flash = require("connect-flash");
 const https = require("https");
 const http = require("http");
 const fs = require("fs");
 const mongoose = require("mongoose");
-const cors = require("cors");
+
+var helmet = require("helmet");
+const User = require("./src/models/User");
 const orderRoute = require("./src/routes/order");
 const adminRoute = require("./src/routes/admin");
 const surveyRoute = require("./src/routes/survey");
 const authRoute = require("./src/routes/auth");
-var helmet = require("helmet");
-// var { redisStore } = require("./src/services/redis");
+var { redisStore } = require("./src/services/redis");
 
 // var options = {
 //   key: fs.readFileSync("/etc/ssl/letsencrypt/smartprint3d.io.key"),
@@ -26,31 +27,32 @@ var helmet = require("helmet");
 
 const app = express();
 const port = 8082;
-const ssl_port = 443;
+// const ssl_port = 443;
+
+app.use(helmet());
+app.use(cors());
+// app.use(
+//   session({
+//     secret: "SmartPrint3Duifsa234jklafdajkqqvnvnzppadfjk",
+//     keys: ["secrefqtkey1", "secretayhqkey2", "fdasfqwrenksdgjlh"],
+//     name: "_smartPrint3Dsess",
+//     // cookie: { secure: true },
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// );
+app.use(flash());
 
 app.use(
   session({
     secret: "SmartPrint3Duifsa234jklafdajkqqvnvnzppadfjk",
-    keys: ["secretkey1", "secretkey2", "fdasfqwrenksdgjlh"],
     name: "_smartPrint3D",
-    cookie: {},
+    cookie: { secure: false },
+    store: redisStore,
     resave: false,
     saveUninitialized: false,
   })
 );
-app.use(cors());
-app.use(flash());
-
-// app.use(
-//   session({
-//     secret: "SmartPrint3Duifsa234jklafdajkqqvnvnzppadfjk",
-//     name: "_smartPrint3D",
-//     cookie: { secure: false },
-//     store: redisStore,
-//     resave: false,
-//     saveUninitialized: false,
-//   })
-// );
 
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "pug");
@@ -76,8 +78,6 @@ app.use(passport.session());
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
-app.use(helmet());
 
 app.use("/api", orderRoute);
 app.use("/admin", adminRoute);
